9a785bdf4bbb0ecf5d750248adb4d2ce8c29ed12
zeng-kun <78148574+zeng-kun@users.noreply.github.com>
2021-05-22 06:37:57 +0800

    Add files via upload
    第二次作业

---
+++ Week_02/作业/OkHttpUtils.java
@@ -0,0 +1,34 @@
+package com.geekbang.week2;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class OkHttpUtils {
+    // 缓存客户端实例
+    public static OkHttpClient client = new OkHttpClient();
+
+    // GET 调用
+    public static String getAsString(String url) throws IOException {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+
+//        String url = "http://www.baidu.com";
+        String url = "http://localhost:8801/homework/week02/helloword";
+        String text = OkHttpUtils.getAsString(url);
+        System.out.println("url: " + url + " ; response: \n" + text);
+
+        // 清理资源
+        OkHttpUtils.client = null;
+    }
+}
